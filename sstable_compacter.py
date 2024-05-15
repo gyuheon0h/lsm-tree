803bb7e4cebeb5df9050f15aeb0216e195c3af90
Gyuheon Oh <goh@Gyuheons-MacBook-Pro.local>
2024-05-15 18:32:04 -0500

    dont load whole file into mem

--- sstable_compacter.py
+++ sstable_compacter.py
@@ -1,3 +1,4 @@
+import heapq
 import struct
 
 class SSTableCompacter:
@@ -7,34 +8,50 @@ class SSTableCompacter:
         self.output_file_path = output_file_path
 
     def compact(self):
-        entries = {}
-        # Read entries from both files
-        for file_path in [self.file_path1, self.file_path2]:
-            with open(file_path, 'rb') as file:
-                # Read until the newline separator
-                while True:
-                    header = file.read(8)  # Read key and offset pair (4 bytes each)
-                    if header == b'\n':
-                        break
-                    key, offset = struct.unpack('ii', header)
-                    # Move to the data offset
-                    current_pos = file.tell()
-                    file.seek(offset)
-                    # Data is a tuple of two doubles
-                    data = struct.unpack('dd', file.read(16))
-                    entries[key] = data
-                    file.seek(current_pos)
+        pq = []
+        files = [open(self.file_path1, 'rb'), open(self.file_path2, 'rb')]
 
-        with open(self.output_file_path, 'wb') as file:
-            offset = 0
-            offset_table = bytearray()
-            data_section = bytearray()
-            for key in sorted(entries.keys()):
-                value = entries[key]
-                value_bytes = struct.pack('dd', *value)
-                offset_table.extend(struct.pack('ii', key, offset))
-                data_section.extend(value_bytes)
-                offset += len(value_bytes)
-            file.write(offset_table)
-            file.write(b'\n')
-            file.write(data_section)
+        # Initialize the priority queue with the first key from each file
+        for i, file in enumerate(files):
+            key, offset = self.read_key_offset(file)
+            if key is not None:
+                heapq.heappush(pq, (key, i, offset))
+
+        with open(self.output_file_path, 'wb') as output_file:
+            last_key, last_data = None, None
+
+            while pq:
+                key, file_index, data_offset = heapq.heappop(pq)
+                data = self.read_data(files[file_index], data_offset)
+
+                # Write to output file
+                if key != last_key:
+                    if last_key is not None:
+                        self.write_key_data(output_file, last_key, last_data)
+                    last_key, last_data = key, data
+
+                # Read next key from the same file
+                next_key, next_offset = self.read_key_offset(files[file_index])
+                if next_key is not None:
+                    heapq.heappush(pq, (next_key, file_index, next_offset))
+
+            # Write the last key
+            if last_key is not None:
+                self.write_key_data(output_file, last_key, last_data)
+
+        for file in files:
+            file.close()
+
+    def read_key_offset(self, file):
+        header = file.read(8)
+        if header == b'\n' or not header:
+            return None, None
+        return struct.unpack('ii', header)
+
+    def read_data(self, file, offset):
+        file.seek(offset)
+        return struct.unpack('dd', file.read(16))
+
+    def write_key_data(self, output_file, key, data):
+        output_file.write(struct.pack('ii', key, len(data)))
+        output_file.write(struct.pack('dd', *data))
